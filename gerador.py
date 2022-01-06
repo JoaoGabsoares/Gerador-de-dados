fbb21aff32bc70af043d2053efb778ef3b5d232d
João Gabriel <joaogabrielgoissoares@gmail.com>
2022-01-05 23:48:43 -0300

    version 1.2

--- gerador.py
+++ gerador.py
@@ -2,11 +2,13 @@ import random
 
 def dados():  
      
-    print('Seja bem vindo, escolha as opções desejadas, obs: caso seja mais de uma , separe por virgula. exemplo, 1,2,3,4. Caso queira parar, digite parar.')
-    opcoes_escolha = input('Escolha as opções desejadas abaixo: \n'
+    print('Seja bem vindo, escolha as opções desejadas, obs: caso seja mais de uma , separe por virgula. exemplo, 1,2,3,4. Caso queira parar, digite "parar". \n')
+   
+   
+    opcoes_escolha = input('Escolha as opções desejadas abaixo \n'
                            '[1]-nomes \n'
                            '[2]-Telefones \n'
-                           '[3]-Emails \n '
+                           '[3]-Emails \n'
                            '[4]-cidades \n'
                            '[5]-estados \n ' 'Escolha o Número desejado: ')
     
@@ -14,34 +16,30 @@ def dados():
     nomes = ['Carlos Augusto','Cristiane Monteiro', 'Enderson Carvalho', 'Marcos da Silva', 'André Pereira']
     estados = ['Rio de janeiro', 'São Paulo', 'Minas Gerais', 'Bahia', 'Rio Grande do Sul']
     #############################################################
+    
     #gerar numeros aleatorios
-    numero_telefone = list(range(0,9))
-    random.shuffle(numero_telefone)
-    #print(numero_telefone)
+    numero_telefone = [5521997849041, 5511946787898, 5523987975897, 5512994788974, 5522994788950]
+    
     #############################################################
+    
     emails = ['adasesa@gmail.com', 'asdasojo@hotmail.com', 'Asdasdxcb@gmail.com', 'skpz@gmail.com', 'sdap@hotmail.com']
-    cidades = ['Rio de janeiro', 'São Paulo', 'Salvador', 'Belo-Horizonte', 'Curitiba']
+    cidades = ['Rio de janeiro', 'São Paulo', 'Salvador', 'Belo-Horizonte', 'Porto Alegre']
     
-    if (opcoes_escolha == '1'):
-        print(random.choice(nomes))
+    for escolha in opcoes_escolha:
+        if escolha == '1':
+            print(random.choice(nomes))
+        elif escolha == '2':
+            print(random.choice(numero_telefone))
+        elif escolha == '3':
+            print(random.choice(emails))
+        elif escolha == '4':
+            print(random.choice(cidades))
+        elif escolha == '5':
+            print(random.choice(estados))
+        elif escolha == 'parar':
+            exit()
         
-    elif (opcoes_escolha == '2'):
-        print( numero_telefone)
-        
-    elif (opcoes_escolha == '3'):
-        print(random.choice(emails))
-    
-    elif (opcoes_escolha == '4'):
-        print(random.choice(cidades))
-    
-    elif (opcoes_escolha == '5'):
-        print(random.choice(estados))
-    
-    elif (opcoes_escolha == 'parar'):
-        exit()
-        
-        
-    
+            
     
 
 dados()
